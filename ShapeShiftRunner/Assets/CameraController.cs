3d8d722bdde2dfa2d67f29eab6da1dd2cfe853e7
Adam <namrog84@gmail.com>
2016-04-15 22:25:35 -0700

    Initial Push start of prototype

---
+++ ShapeShiftRunner/Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+    
+    private float offsetX = 14;
+    private float damping = 1.0f;
+    private float velocity = 0;
+
+    // Update is called once per frame
+    void Update () {
+        var pos = transform.position;
+        float currentX = pos.x - offsetX;
+        float targetX = player.transform.position.x;
+        
+        currentX = Mathf.SmoothDamp(currentX, targetX, ref velocity, 0.05f, 15);
+
+        pos.x = currentX + offsetX;
+
+
+        transform.position = pos;
+        
+	}
+}
---
+++ ShapeShiftRunner/Assets/MainPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MainPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public float horizontalAccel = 10.0f;
+    private float gravity = -9.8f;
+    public Vector3 speed = new Vector3(0, 0);
+    private float maxSpeed = 13.0f;
+
+
+    public Sprite SquareSprite;
+    public Sprite TriangleSprite;
+    public Sprite CircleSprite;
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GetComponent<SpriteRenderer>().sprite = SquareSprite;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GetComponent<SpriteRenderer>().sprite = TriangleSprite;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            GetComponent<SpriteRenderer>().sprite = CircleSprite;
+        }
+
+        //pos.x += speed * Time.deltaTime;
+
+        //pos.y += gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+        speed.x += horizontalAccel * Time.deltaTime;
+        speed.x = Mathf.Min(speed.x, maxSpeed);
+
+        speed.y += 2.5f * gravity * Time.deltaTime;
+
+        var pos = transform.position;
+        pos += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void Jump()
+    {
+        speed.y = 12f;
+    }
+}
